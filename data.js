316772aef833607826e484288c271a4aca1c14a6
crisner1978 <crisner.prsvr@gmail.com>
2021-10-20 13:10:41 -0500

    make it better

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        url:"https://v39-us.tiktokcdn.com/4462599c7481ece51da4a0c7a9efdd61/616fec53/video/tos/useast2a/tos-useast2a-ve-0068c004/83fdfa84000b45809bf952d347326618/?a=1233&br=2834&bt=1417&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=98Cx~eta4kag3&l=20211020041531010189196070200298D7&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=Mzk7ODQ6ZmxkODMzNzczM0ApZWZkMzk2OmU2NzpoNzxkZ2cxM2dtcjQwZmZgLS1kMTZzc2AwYjRgMzJjNS0vYy5jMjE6Yw%3D%3D&vl=&vr=",
+                channel:"crisner",
+                song:"Shake it",
+                description:"shakes and shakes",
+                likes:"432",
+                messages:"321",
+                shares:"654",
+    },
+    {
+        url:"https://v16m-us.tiktokcdn.com/06f8e47d315c8b1ffb84df2bacee5cd9/61700a6d/video/tos/alisg/tos-alisg-pve-0037c001/06d44a1c1abc4938b8fafd59dd46e147/?a=1233&br=3726&bt=1863&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=98Cx~eta4kag3&l=202110200623550101890722182605610B&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3h4bjM6Zjo2ODMzODczNEApaGY4Ozk6ODxoN2g1OTZnM2czcWBgcjRnYTNgLS1kMS1zc2EvYmIuNmI2Yi0tMTAxMDQ6Yw%3D%3D&vl=&vr=",
+                channel:"danceparty",
+                song:"Electric Beat",
+                description:"electric boogaloo",
+                likes:"789",
+                messages:"314",
+                shares:"654",
+    }
+]
+
+export default data;
---
+++ server.js
@@ -0,0 +1,58 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import data from "./data.js";
+import Videos from "./dbModel.js";
+
+// app Config
+
+const app = express();
+const port = 9000;
+dotenv.config();
+
+// Middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"),
+    res.setHeader("Access-Control-Allow", "*"),
+    next();
+});
+
+// DB Config
+const db_url = process.env.DB_URI;
+mongoose.connect(db_url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// API Endpoints
+
+app.get("/", (req, res) => res.status(200).send("hello world"));
+
+app.get("/v1/posts", (req, res) => res.status(200).send(data));
+
+app.get("/v2/posts", (req, res) => {
+  Videos.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/v2/posts", (req, res) => {
+  const dbVideos = req.body;
+
+  Videos.create(dbVideos, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+// Listener
+
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
