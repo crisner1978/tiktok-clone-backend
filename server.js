b16c44d438a6567731d938e2566c996456c8683e
crisner1978 <crisner.prsvr@gmail.com>
2021-10-20 15:15:39 -0500

    make it work

--- server.js
+++ server.js
@@ -14,9 +14,11 @@ dotenv.config();
 app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*"),
-    res.setHeader("Access-Control-Allow-Headers", "*"),
-      res.setHeader("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS")
-    next();
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+  next();
 });
 
 // DB Config
